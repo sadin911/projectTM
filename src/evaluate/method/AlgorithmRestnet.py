c67799e09b89fa9bd96c398ac2abd353b40a7b7b
NHSO\watcharapong.c <ne7shii@hotmail.com>
2021-03-19 10:49:32 +0700

    add batch feature extrac to restnet

--- src/evaluate/method/AlgorithmRestnet.py
+++ src/evaluate/method/AlgorithmRestnet.py
@@ -6,6 +6,7 @@ import numpy as np
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import GlobalAveragePooling2D
 from PIL import Image
+from sklearn.preprocessing import normalize
 
 class Restnet:
     def __init__(self):
@@ -28,8 +29,25 @@ class Restnet:
         out = self.model.predict(indput_data)[0]
         normalized_v = out / np.sqrt(np.sum(out**2))
         
-    
-        return normalized_v
+    def feature_extract_batch(self,image_path_list,batch_size):
+        image_pool = image_path_list
+        result_list=[]
+        batch_data=[]
+        while(len(image_pool)>0):
+            temp= image_pool.pop(0)
+            img_pil = Image.open(temp).convert('RGB')
+            img_pil = img_pil.resize((224,224))
+            img_cv = np.asarray(img_pil)
+            indput_data = tf.keras.applications.resnet.preprocess_input(img_cv)
+            batch_data.append(indput_data)
+            if(len(batch_data)==batch_size or len(image_pool)==0):
+                batch_result = self.model.predict(np.array(batch_data))
+                batch_data=[]
+                result_list.extend(batch_result)
+        # indput_data = np.expand_dims(indput_data, axis = 0)
+        result = normalize(np.array(result_list), norm="l2")
+
+        return result
     
     def match_1to1(self,template1,template2):
         score = np.dot(template1, template2)
@@ -60,8 +78,9 @@ if __name__ == '__main__':
     print("main")
     cat = Restnet()
     feature = cat.feature_extract(r"D:\project\trademark\projectTM\src\imageGroupALL\2d_160115081\0.jpg")
-    cat.match_1to1(feature, feature)
-    for i in range(1000):
-        cat.enroll_to_DB(feature,str(i))
-    cat.enroll()
-    (_id,score) = cat.retriev(feature, 100)
+    feature = cat.feature_extract_batch([r"D:\project\trademark\projectTM\src\imageGroupALL\2d_160115081\0.jpg" for x in range(100)], 30)
+    # cat.match_1to1(feature, feature)
+    # for i in range(1000):
+    #     cat.enroll_to_DB(feature,str(i))
+    # cat.enroll()
+    # (_id,score) = cat.retriev(feature, 100)
